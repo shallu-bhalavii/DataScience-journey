7efbf7e29902f6976bdb55e4e99683bffd32c414
shallu bhalavii <techrose.13@gmail.com>
2025-08-11 19:04:42 +0530

    update

--- day2_ds.py
+++ day2_ds.py
@@ -1,18 +1,18 @@
 # Day 2: Python Data Structures Practice
 
-# 1. List example
+# 1. List example  (ordered, allows duplicate,changeable)mutable
 fruits = ["apple", "banana", "cherry"]
 fruits.append("mango")
 print("List of fruits:", fruits)
 
-# 2. Tuple example
+# 2. Tuple example  (ordered,immutable,allows duplicate )
 coordinates = (10, 20)
 print("Coordinates:", coordinates)
 
-# 3. Set example
+# 3. Set example    (unordered,changeable, no duplicate members and cannot accesed with index)
 unique_numbers = {1, 2, 3, 3, 2}
 print("Unique numbers:", unique_numbers)
 
-# 4. Dictionary example
-student = {"name": "Shallu", "age": 22, "course": "Data Science"}
+# 4. Dictionary example (ordered,mutable and can't have duplicate key❌ bt value✔️)
+student = {"name": "Shallu", "age": 20, "course": "Data Science"}
 print("Student Info:", student)
