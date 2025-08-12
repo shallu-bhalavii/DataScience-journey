65291e680c2f49a0cc9ff31572bfa1f43760bbde
shallu bhalavii <techrose.13@gmail.com>
2025-08-12 17:39:21 +0530

    add update

--- day2_ds.py
+++ day2_ds.py
@@ -4,15 +4,21 @@
 fruits = ["apple", "banana", "cherry"]
 fruits.append("mango")
 print("List of fruits:", fruits)
+print(fruits[0])
+
 
 # 2. Tuple example  (ordered,immutable,allows duplicate )
 coordinates = (10, 20)
 print("Coordinates:", coordinates)
+print(coordinates[1])
+
 
 # 3. Set example    (unordered,changeable, no duplicate members and cannot accesed with index)
 unique_numbers = {1, 2, 3, 3, 2}
 print("Unique numbers:", unique_numbers)
+# print(unique_numbers[3])  #error
 
 # 4. Dictionary example (ordered,mutable and can't have duplicate key❌ bt value✔️)
 student = {"name": "Shallu", "age": 20, "course": "Data Science"}
 print("Student Info:", student)
+print(student["age"])  #dictionary access by key not index
